9dd3457f03812ff0761bd07bed9c2adef0b09430
adideswilliams <adides@gmail.com>
2016-03-19 12:59:55 -0700

    socket io added for client and server

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ var app = express();
 //tell node to start a new server then use app as boilerplate 
 var http = require ('http').Server(app);
 
+//bring in http server with socket.io library
+var io = require('socket.io')(http);
+
 //use express static to expose folder
 //express static takes file path as only argument 
 app.use(express.static(__dirname + '/public'));
+
+//on is to listen to event, first argument is name of the event
+io.on('connection', function(){
+	console.log('User connected via socket.io');
+});
 
 //start server
 http.listen(PORT, function (){
